5dd11922a1acfd30b701ee6f444be09649199c66
Heather Faerber <heather@turing.com>
2022-05-09 10:32:13 -0600

    Add feature

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 
 
-//Lets pretend there is some top notch code here that completes our "Search By Tag" feature.  Now we can push it, create a PR, have it reviewed and once its approved and ready, we can merge it and close that GH Issue  
+//Lets pretend there is some top notch code here that completes our "Search By Tag" feature.  Now we can push it, create a PR, have it reviewed and once its approved and ready, we can merge it and close that GH Issue
+
+//test
